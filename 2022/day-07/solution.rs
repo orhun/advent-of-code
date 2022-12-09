7cc6d140a1cd74f18633de472c30ecbd5cc53724
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-12-09 18:43:50 +0300

    Improve day-07

--- 2022/day-07/solution.rs
+++ 2022/day-07/solution.rs
@@ -1,19 +1,91 @@
 #!/usr/bin/env rust-script
 
 use std::error::Error;
-use std::fmt::Write;
-use std::fs::{self, File};
-use std::io::Write as IoWrite;
+use std::fs;
 use std::path::PathBuf;
 use std::str::FromStr;
 
 const INPUT_FILE: &str = "input";
+
+#[derive(Debug, Default)]
+struct Entry {
+    name: String,
+    type_: EntryType,
+}
+
+impl Entry {
+    fn new(line: &str) -> Option<Self> {
+        let parts = line.split_whitespace().collect::<Vec<&str>>();
+        if parts.first()?.starts_with("dir") {
+            Some(Self {
+                name: parts.get(1)?.to_string(),
+                type_: EntryType::Directory,
+            })
+        } else {
+            Some(Self {
+                name: parts.get(1)?.to_string(),
+                type_: EntryType::File(parts.first().and_then(|v| v.parse().ok())?),
+            })
+        }
+    }
+}
+
+#[derive(Debug)]
+enum EntryType {
+    Directory,
+    File(u32),
+    Unknown,
+}
+
+impl Default for EntryType {
+    fn default() -> Self {
+        Self::Unknown
+    }
+}
+
+#[derive(Debug, Default)]
+struct Directory {
+    name: String,
+    entries: Vec<Entry>,
+}
+
+impl Directory {
+    fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            ..Default::default()
+        }
+    }
+
+    fn calculate_size(&self, directories: &[Directory]) -> u32 {
+        self.entries.iter().fold(0, |mut sum, entry| {
+            match entry.type_ {
+                EntryType::Directory => {
+                    sum += directories
+                        .iter()
+                        .find(|dir| dir.name == entry.name)
+                        .unwrap()
+                        .calculate_size(directories)
+                }
+                EntryType::File(size) => sum += size,
+                EntryType::Unknown => {}
+            }
+            sum
+        })
+    }
+}
 
 #[derive(Debug)]
 enum Command {
     ChangeDirectory(String),
     List,
     Unknown,
+}
+
+impl Default for Command {
+    fn default() -> Self {
+        Self::Unknown
+    }
 }
 
 impl FromStr for Command {
@@ -33,118 +105,62 @@ impl FromStr for Command {
     }
 }
 
-#[derive(Debug)]
-struct Entry {
-    name: String,
-    type_: EntryType,
-}
-
-impl FromStr for Entry {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts = s.split_whitespace().collect::<Vec<&str>>();
-        if parts.first().ok_or(())?.starts_with("dir") {
-            Ok(Self {
-                name: parts.get(1).ok_or(())?.to_string(),
-                type_: EntryType::Directory,
-            })
-        } else {
-            Ok(Self {
-                name: parts.get(1).ok_or(())?.to_string(),
-                type_: EntryType::File(parts.first().and_then(|v| v.parse().ok()).ok_or(())?),
-            })
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-enum EntryType {
-    Directory,
-    File(u32),
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string(INPUT_FILE)?;
-    let mut script = String::new();
-    writeln!(script, "#!/usr/bin/env bash")?;
+
+    let mut directories = Vec::<Directory>::new();
+    let mut current_directory = Directory::default();
+    let mut current_level = PathBuf::new();
     let mut current_command = Command::Unknown;
     for line in input.lines() {
         if line.starts_with('$') {
-            current_command = Command::from_str(line).expect("failed to parse command");
+            current_command = Command::from_str(line).unwrap();
         }
         match current_command {
             Command::ChangeDirectory(ref name) => {
-                if name == "/" {
-                    writeln!(script, "{}", r#"tempdir="$(mktemp -d)""#)?;
-                    writeln!(script, "{}", r#"cd "$tempdir""#)?;
-                } else {
-                    writeln!(script, "cd {name}")?;
+                if name == ".." {
+                    current_level.pop();
+                    continue;
                 }
+                if !current_directory.name.is_empty() {
+                    directories.push(current_directory);
+                }
+                current_level.push(name);
+                current_directory = Directory::new(&current_level.to_string_lossy());
             }
             Command::List => {
                 if line.starts_with('$') {
                     continue;
                 }
-                let entry = Entry::from_str(line).expect("failed to parse entry");
-                match entry.type_ {
-                    EntryType::Directory => {
-                        writeln!(script, "mkdir {}", entry.name)?;
-                    }
-                    EntryType::File(size) => {
-                        writeln!(script, "echo {} > {}", size, entry.name)?;
-                    }
-                }
+                let mut entry = Entry::new(line).unwrap();
+                entry.name = current_level.join(entry.name).to_string_lossy().to_string();
+                current_directory.entries.push(entry);
             }
             Command::Unknown => {
                 eprintln!("Unknown command: {line}");
             }
         }
     }
-    let mut script1 = script.clone();
-    let mut script2 = script.clone();
-    writeln!(
-        script1,
-        "{}",
-        r#"
-cd "$tempdir"
-result=0
-readarray -t dirs <<<"$(find . -type d)"
-for dir in "${dirs[@]}"; do
-	readarray -t files <<<"$(find $dir -type f)"
-	output=$(awk '{arr[FNR]+=$1}END{for(i=1;i<=FNR;i+=1){print arr[i]}}' "${files[@]}")
-	if [ "$output" -lt 100000 ]; then
-		result=$((result + output))
-	fi
-done
-echo "$result""#
-    )?;
-    writeln!(
-        script2,
-        "{}",
-        r#"
-cd "$tempdir"
-result=()
-readarray -t dirs <<<"$(find . -type d)"
-for dir in "${dirs[@]}"; do
-	readarray -t files <<<"$(find $dir -type f)"
-	output=$(awk '{arr[FNR]+=$1}END{for(i=1;i<=FNR;i+=1){print arr[i]}}' "${files[@]}")
-	result+=("$output")
-done
-IFS=$'\n'
-total=$(echo "${result[*]}" | sort -nr | head -n1)
-readarray -t sorted_result <<<"$(echo "${result[*]}" | sort -n)"
-for v in "${sorted_result[@]}"; do
-	if ((v + 70000000 - total > 30000000)); then
-		echo "$v"
-		break
-	fi
-done"#
-    )?;
-    for (mut i, script) in [script1, script2].iter().enumerate() {
-        i += 1;
-        let solution_path = PathBuf::from(format!("generated_solution{i}.sh"));
-        File::create(&solution_path)?.write_all(script.as_bytes())?;
-        println!("Part{i} saved to: {}", solution_path.to_string_lossy());
+    directories.push(current_directory);
+    let mut total_sizes = Vec::new();
+    for directory in directories.iter() {
+        let total_size = directory.calculate_size(&directories);
+        total_sizes.push(total_size);
     }
+    total_sizes.sort();
+
+    println!(
+        "Part1: {}",
+        total_sizes.iter().filter(|v| v < &&100000).sum::<u32>()
+    );
+
+    println!(
+        "Part2: {}",
+        total_sizes
+            .iter()
+            .find(|v| *v + 70000000 - total_sizes[total_sizes.len() - 1] > 30000000)
+            .unwrap()
+    );
+
     Ok(())
 }
